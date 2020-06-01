615207ce134226f34bd204db343f0705e9e57fb3
Saumya Upadhyay <saumyaupadhyay@Saumyas-MacBook-Air.local>
2020-06-01 11:57:02 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,129 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Form, Field } from "react-final-form";
+import { TextField } from "final-form-material-ui";
+import {
+  Typography,
+  Paper,
+  Grid,
+  Button,
+  CssBaseline,
+} from "@material-ui/core";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const onSubmit = async (values) => {
+  //const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  //await sleep(300);
+  window.alert(JSON.stringify(values, 0, 3));
+  //const axios = require("axios");
+  console.log(values);
+  const API_URL =
+    "https://bzsx72vxd1.execute-api.us-east-1.amazonaws.com/default/sendEmailMessageFunction";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": true,
+    },
+    body: JSON.stringify({
+      name: values.firstName,
+      email: values.email,
+      message: values.message,
+    }),
+  }).then(function (response) {
+    console.log(response);
+  });
+};
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  }
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  }
+  if (!values.message) {
+    errors.message = "Required";
+  }
+  return errors;
+};
+
+function App() {
+  return (
+    <div style={{ padding: 16, margin: "auto", maxWidth: 600 }}>
+      <CssBaseline />
+      <Typography variant="h4" align="center" component="h1" gutterBottom>
+        MK Challenge Form
+      </Typography>
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        render={({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit} noValidate>
+            <Paper style={{ padding: 16 }}>
+              <Grid container alignItems="flex-start" spacing={2}>
+                <Grid item xs={6}>
+                  <Field
+                    fullWidth
+                    required
+                    name="firstName"
+                    component={TextField}
+                    type="text"
+                    label="First Name"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Field
+                    fullWidth
+                    required
+                    name="lastName"
+                    component={TextField}
+                    type="text"
+                    label="Last Name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    name="email"
+                    fullWidth
+                    required
+                    component={TextField}
+                    type="email"
+                    label="Email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    name="message"
+                    fullWidth
+                    required
+                    component={TextField}
+                    type="text"
+                    label="Message"
+                  />
+                </Grid>
+
+                <Grid item style={{ marginTop: 16 }}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    disabled={submitting}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Paper>
+          </form>
+        )}
+      />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
